f29bb75f52d1ccd00b6b786512cdcb5724ff76ea
jayzizzle <dj.jonzamora@gmail.com>
2021-10-09 11:08:26 -0700

    add passport

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const mongoose = require('mongoose');
 const express = require("express");
+const app = express();
 const bodyParser = require('body-parser');
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const User = require('./models/User')
+const db = require('./config/keys').mongoURI;
 
-const app = express();
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.get("/", (req, res) => res.send("Excelsior"));
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
-const db = require('./config/keys').mongoURI;
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
