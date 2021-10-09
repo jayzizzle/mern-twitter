0b1213aa191de6b48139e0d0b97654744cf54330
jayzizzle <dj.jonzamora@gmail.com>
2021-10-09 11:59:36 -0700

    add validation files

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,9 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const { secretOrKey } = require("../../config/keys");
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
@@ -48,7 +51,22 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
-            res.json({msg: 'Success'});
+            const payload = {
+              id: user.id,
+              handle: user.handle,
+              email: user.email
+            }
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              { expiresIn: 3600 },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                });
+              }
+            )
           } else {
             return res.status(400).json({password: 'Incorrect password'});
           }
