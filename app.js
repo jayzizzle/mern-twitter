7b4e9b5be4501c0d6a917f839313f17773d89d21
jayzizzle <dj.jonzamora@gmail.com>
2021-10-08 22:25:38 -0700

    add mongoose and express

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 const express = require("express");
+const bodyParser = require('body-parser');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get("/", (req, res) => res.send("Excelsior"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 const db = require('./config/keys').mongoURI;
 mongoose
   .connect(db, { useNewUrlParser: true })
