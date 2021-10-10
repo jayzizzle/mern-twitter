3e585ca0c1b551eb8e0159bf2f0a5aaa9735b4c2
jayzizzle <dj.jonzamora@gmail.com>
2021-10-09 18:08:45 -0700

    prep app for heroku push

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const tweets = require("./routes/api/tweets");
 const User = require('./models/User')
 const db = require('./config/keys').mongoURI;
 const passport = require('passport');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true })
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 // We will create this component shortly
 import Root from './components/root';
@@ -46,7 +47,12 @@ document.addEventListener('DOMContentLoaded', () => {
     store = configureStore({});
   }
   // Render our root component and pass in the store as a prop
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  
   const root = document.getElementById('root');
-
+  
   ReactDOM.render(<Root store={store} />, root);
 });
+
+window.axios = axios;
